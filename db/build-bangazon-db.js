3db9283422050400ddb314dbb0340dbe50311ae1
Greg Korte <gregmkorte@gmail.com>
2017-09-27 10:00:13 -0500

    Took our all references to hr side from bangazon-api.

--- db/build-bangazon-db.js
+++ db/build-bangazon-db.js
@@ -10,13 +10,7 @@ const tableNames = [
   'orders',
   'product_types',
   'products',
-  'orders_products',
-  'departments',
-  'employees',
-  'training_programs',
-  'employees_training_programs',
-  'computers',
-  'employees_computers'
+  'orders_products'
 ];
 
 // function to drop tables if they exist to reset db
@@ -28,7 +22,6 @@ const dropAllTables = (tableArray) => {
 
 // drops all tables
 dropAllTables(tableNames);
-
 
 // table creation
 
@@ -52,24 +45,4 @@ db.serialize(function() {
   // create orders_products table
   db.run('CREATE TABLE IF NOT EXISTS orders_products (order_product_id INTEGER PRIMARY KEY, order_id INTEGER, product_id INTEGER, FOREIGN KEY (order_id) REFERENCES orders(order_id), FOREIGN KEY (product_id) REFERENCES products(product_id))');
 
-
-  // hr side
-
-  // create departments table
-  db.run('CREATE TABLE IF NOT EXISTS departments (department_id INTEGER PRIMARY KEY, name TEXT, supervisor_id INTEGER)');
-
-  // create employees table
-  db.run('CREATE TABLE IF NOT EXISTS employees (employee_id INTEGER PRIMARY KEY, first_name TEXT, last_name TEXT, department_id INTEGER, FOREIGN KEY (department_id) REFERENCES departments(department_id))');
-
-  // create training_programs table
-  db.run('CREATE TABLE IF NOT EXISTS training_programs (training_program_id INTEGER PRIMARY KEY, name TEXT, start_date TEXT, end_date TEXT, number_enrolled INTEGER)');
-
-  // create employees_training_programs table
-  db.run('CREATE TABLE IF NOT EXISTS employees_training_programs (employee_training_program_id INTEGER PRIMARY KEY, employee_id INTEGER, training_program_id INTEGER, FOREIGN KEY (employee_id) REFERENCES employees(employee_id), FOREIGN KEY (training_program_id) REFERENCES training_programs(training_program_id))');
-
-  // create computers table
-  db.run('CREATE TABLE IF NOT EXISTS computers (computer_id INTEGER PRIMARY KEY, begin_service_date TEXT, end_service_date TEXT)');
-
-  // create employees_computers table
-  db.run('CREATE TABLE IF NOT EXISTS employees_computers (employee_computer_id INTEGER PRIMARY KEY, start_date TEXT, end_date TEXT, employee_id INTEGER, computer_id INTEGER, FOREIGN KEY (employee_id) REFERENCES employees(employee_id), FOREIGN KEY (computer_id) REFERENCES computers(computer_id))');
 })
